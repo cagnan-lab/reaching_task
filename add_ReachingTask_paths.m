986dc1f83794d0dd3dabb67408553ef75e2b96ee
Tim West <twestWTCN@users.noreply.github.com>
2019-02-13 13:56:13 +0000

    InitialTaskCode
    Added basic reaching task code for cogent

---
+++ add_ReachingTask_paths.m
@@ -0,0 +1,14 @@
+function add_ReachingTask_paths()
+if strcmp(getenv('COMPUTERNAME'),'SFLAP-2')
+    gitpath = 'C:\Users\Tim\Documents\Work\GIT';
+    mapath = 'C:\Users\Tim\Documents\MATLAB_ADDONS';
+    
+elseif strcmp(getenv('COMPUTERNAME'),'FREE')
+    gitpath = 'C:\Users\twest\Documents\Work\Github';
+    mapath = 'C:\Users\twest\Documents\Work\MATLAB ADDONS';
+end
+
+addpath(genpath([gitpath '\reaching_task']))
+addpath([mapath '\Cogent2000v1.33\Samples'])
+addpath([mapath '\Cogent2000v1.33\Toolbox'])
+addpath([mapath '\TWtools'])
---
+++ task_code/master_script.m
@@ -0,0 +1,58 @@
+% Reaching Task Master Script
+% Setup Images
+baseTar = {
+    'C:\Users\Tim\Documents\Work\GIT\reaching_task\task_images\targets\fixation_cross_targets.bmp';...
+    'C:\Users\Tim\Documents\Work\GIT\reaching_task\task_images\targets\0_0_0_0_targets.bmp';...
+    };
+selTar = {
+    'C:\Users\Tim\Documents\Work\GIT\reaching_task\task_images\targets\1_0_0_0_targets.bmp';...
+    'C:\Users\Tim\Documents\Work\GIT\reaching_task\task_images\targets\0_1_0_0_targets.bmp';...
+    'C:\Users\Tim\Documents\Work\GIT\reaching_task\task_images\targets\0_0_1_0_targets.bmp';...
+    'C:\Users\Tim\Documents\Work\GIT\reaching_task\task_images\targets\0_0_0_1_targets.bmp';...
+    };
+
+% Setup Display
+% config_display(1, 6, [0 0 0], [1 1 1], 'Helvetica', 50, 4, 24);
+config_display(0,6, [1 1 1],[0 0 0])
+config_log( 'reachingTask.log' ); % Configure log file
+start_cogent;
+
+% Fixation cross
+clearpict( 1 );
+loadpict( baseTar{1}, 1 );
+
+% Target Selection
+clearpict( 3 );
+loadpict( baseTar{2}, 3 );
+
+txt_welcomeStr = 'Get ready to reach!!!';
+preparestring( txt_welcomeStr, 2 ); % Put word in buffer 1
+% Draw Welcome Text
+t0 = drawpict( 2 );
+waituntil( t0 + 2000 );
+
+% Setup rng seed (so to make reproducible sequence)
+rng(14231)
+% Setup number of trials
+ntrials = 10;
+% Setup random vector of targets
+tarlist = randi(4,ntrials);
+
+for i = 1:ntrials
+    % Fixation Cross
+    t(1) = drawpict( 1 ); % Display fixation cross
+    waituntil( t(1) + 1500 ); % Display fixation point for 1000ms
+    
+    % Target Range
+    t(2) = drawpict( 3 ); % Display target range
+    
+    % Target Choice
+    r = randnbetween(2500,1000);
+    waituntil( t(2)+ r);
+    clearpict( 4 );
+    loadpict( selTar{tarlist(i)}, 4 );
+    t(3) = drawpict( 4 ); % Display word and get the time
+    waituntil(t(3)+ 2000 );
+    
+end
+stop_cogent;
