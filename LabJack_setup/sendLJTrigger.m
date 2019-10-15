9b44e1e373368e1a2e9792799230a41479ee2227
Tim West <twestWTCN@users.noreply.github.com>
2019-10-15 12:07:12 +0100

    LJ_channel update

--- LabJack_setup/sendLJTrigger.m
+++ LabJack_setup/sendLJTrigger.m
@@ -1,10 +1,10 @@
 function ljudObj = sendLJTrigger(ljudObj,ljhandle,v,c)
 if nargin<4
-    c = 0;
+    c = 1;
 end
 % Set DAC0 to 3.0 volts.
 channel = c;
 voltage = v;
 binary = 0;
 ljudObj.eDAC(ljhandle, 0, voltage, binary, 0, 0);
-disp(['DAC0 set to ' num2str(voltage) ' V'])
+disp(['DAC1 set to ' num2str(voltage) ' V'])
