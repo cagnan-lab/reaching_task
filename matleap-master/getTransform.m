4d83cde74f861990dc05fd74d444957fcfbac9f5
Tim West NDCN <timothy.west@ndcn.ox.ac.uk>
2019-11-28 17:34:17 +0000

    Update_CAlibration

---
+++ matleap-master/getTransform.m
@@ -0,0 +1,13 @@
+function [XKey,YKey] = getTransform(WESN_app,WESN_leap)
+% WESN is [XWest XEast YSouth YNorth]
+Xrange_app = WESN_app(2)-WESN_app(1);
+Xrange_leap = WESN_leap(2)-WESN_leap(1);
+
+Yrange_app = WESN_app(4)-WESN_app(3);
+Yrange_leap = WESN_leap(4)-WESN_leap(3);
+
+% xS = (xL- WESN_leap(1))*(Xrange_app/Xrange_leap) + WESN_app(1)
+XKey = [WESN_leap(1) (Xrange_app/Xrange_leap) WESN_app(1)];
+YKey = [WESN_leap(3) (Yrange_app/Yrange_leap) WESN_app(3)];
+
+
