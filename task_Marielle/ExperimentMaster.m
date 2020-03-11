7b901e589a299c9499936b9ffcb51f5d02830504
mariellestam <mariellestam@hotmail.nl>
2020-03-11 21:23:58 +0000

    Update ExperimentMaster

--- task_Marielle/ExperimentMaster.m
+++ task_Marielle/ExperimentMaster.m
@@ -24,14 +24,14 @@ tracking        = '0';          % 0 = Index Finger Tip, 1 = Palm Pos, (2 = Stab 
 labjack         = '0';          % 1 = Connected, 0 = Not Connected                          bool
 tick            = '0';          % 1 = Connected, 0 = Not Connected                          bool
 pointervisible  = '1';          % 1 = Visible, 0 = Not Visible                              bool
-reaches         = '5';          % Amount of Reaches per Trial                               int
+reaches         = '20';         % Amount of Reaches per Trial                               int
 % uncertainty     = '2';          % 0 = one Arrow, 1 = four Arrows, 2 = eight Arrows          bool
-condinfo        = '2';          % Seconds of showing Condition Information                  float
-breakreaches    = '2';         % Amount of Continuous Reaches before break                 int
-breakwait       = '3';         % Seconds of break after 10 reaches                         float
-posturalhold    = '2';         % Seconds of PosturalHold Task                              float
-rest            = '2';         % Seconds of Rest Task                                      float
-posturestart    = '2';          % Seconds of Postural Hold before Trials                    float
+condinfo        = '5';          % Seconds of showing Condition Information                  float
+breakreaches    = '10';         % Amount of Continuous Reaches before break                 int
+breakwait       = '5';          % Seconds of break after 10 reaches                         float
+posturalhold    = '10';         % Seconds of PosturalHold Task                              float
+rest            = '10';         % Seconds of Rest Task                                      float
+posturestart    = '5';          % Seconds of Postural Hold before Trials                    float
 reachwait       = '2.5';        % Seconds of Reach Wait                                     float
 prepwait        = '2.5';        % Seconds of Motor Preparation Wait                         float
 delaywait       = '3.5';        % Seconds of Motor Execution Wait GBYK                      float
@@ -44,7 +44,7 @@ mkdir(confpath)
 configuration(confpath, tracking, labjack, tick, pointervisible, reaches, condinfo, breakreaches, breakwait, posturalhold, rest, posturestart, reachwait, prepwait, delaywait, holdwait, colorduration)
 
 %% Calibrate the LeapMotion to Screen Space
-uiopen([cd '\Unity Builds\Calibration\Calibration.exe'],1)
+uiopen([cd '\Unity Builds\11.03\Calibration\Calibration.exe'],1)
 close all;
 
 %% Run Conditions
@@ -61,37 +61,16 @@ for rep = 1:2
         fprintf(fileCONDITION,num2str(cond));
         fclose(fileCONDITION);
         
-        disp('Press to create CURRID')
-        pause
         % Made ConditionID path
         currid = [subcode '_Condition_' num2str(cond) '_Rep_' num2str(rep)];
         fileCURRID = fopen([confpath '\CURRID.txt'],'w');
         fprintf(fileCURRID,currid);
         fclose(fileCURRID);
         
-        %         %     PostureHold(id,block,15)
-        disp('Press to start PosturalHold')
-        pause
-        uiopen([cd '\Unity Builds\PosturalHold\PosturalHold.exe'],1)
-        close all;
-        
-        %         %     Rest(id,block,15)
-        disp('Press to start Rest')
-        pause
-        uiopen([cd '\Unity Builds\Rest\Rest.exe'],1)
-        close all;
-        
         disp(['Press to start Condition ', num2str(cond)])
         pause
-        if cond == 1
-            uiopen([cd '\Unity Builds\02.03 Last Updated Tasks!!\Condition 1\Condition 1.exe'],1)
-        elseif cond == 2
-            uiopen([cd '\Unity Builds\Condition 2\Condition 2.exe'],1)
-        elseif cond == 3
-            uiopen([cd '\Unity Builds\Condition 3\Condition 3.exe'],1)
-        elseif cond == 4
-            uiopen([cd '\Unity Builds\Condition 4\Condition 4.exe'],1)
-        end
+        uiopen([cd '\Unity Builds\11.03\ConditionsBetter\AllConditionsBetter.exe'],1)
+        close all;
         
         trialComments = input('Any comments?:    ','s');
         fileCOMMENTS = fopen([subjectpath '\' CURRID '\' subcode '_TrialComments.txt'],'w');
