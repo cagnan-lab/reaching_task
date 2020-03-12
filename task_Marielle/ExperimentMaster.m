e7127d37e1102139b587f85cb9bb16bf25c3593e
mariellestam <57631979+mariellestam@users.noreply.github.com>
2020-03-12 11:18:54 +0000

    finito

--- task_Marielle/ExperimentMaster.m
+++ task_Marielle/ExperimentMaster.m
@@ -24,15 +24,15 @@ tracking        = '0';          % 0 = Index Finger Tip, 1 = Palm Pos, (2 = Stab 
 labjack         = '0';          % 1 = Connected, 0 = Not Connected                          bool
 tick            = '0';          % 1 = Connected, 0 = Not Connected                          bool
 pointervisible  = '1';          % 1 = Visible, 0 = Not Visible                              bool
-reaches         = '20';         % Amount of Reaches per Trial                               int
+reaches         = '20';         % Total amount of Reaches per Trial                         int
 % uncertainty     = '2';          % 0 = one Arrow, 1 = four Arrows, 2 = eight Arrows          bool
 condinfo        = '5';          % Seconds of showing Condition Information                  float
 breakreaches    = '10';         % Amount of Continuous Reaches before break                 int
 breakwait       = '5';          % Seconds of break after 10 reaches                         float
-posturalhold    = '10';         % Seconds of PosturalHold Task                              float
-rest            = '10';         % Seconds of Rest Task                                      float
-posturestart    = '5';          % Seconds of Postural Hold before Trials                    float
-reachwait       = '2.5';        % Seconds of Reach Wait                                     float
+posturalhold    = '60';         % Seconds of PosturalHold Task                              float
+rest            = '60';         % Seconds of Rest Task                                      float
+posturestart    = '10';         % Seconds of Postural Hold before Trials                    float
+reachwait       = '3.5';        % Seconds of Reach Wait                                     float
 prepwait        = '2.5';        % Seconds of Motor Preparation Wait                         float
 delaywait       = '3.5';        % Seconds of Motor Execution Wait GBYK                      float
 holdwait        = '4';          % Seconds of Motor Execution Wait GAYK                      float
@@ -52,7 +52,7 @@ close all;
 for rep = 1:2
     % Determine random order of conditions:
     condition = randperm(4);
-    
+     
     for block = 1:4
         cond = condition(block);
         
@@ -69,15 +69,14 @@ for rep = 1:2
         
         disp(['Press to start Condition ', num2str(cond)])
         pause
-        uiopen([cd '\Unity Builds\11.03\ConditionsBetter\AllConditionsBetter.exe'],1)
+        uiopen([cd '\Unity Builds\12.03\AllConditions\AllConditions.exe'],1)
         close all;
         
         trialComments = input('Any comments?:    ','s');
-        fileCOMMENTS = fopen([subjectpath '\' CURRID '\' subcode '_TrialComments.txt'],'w');
+        fileCOMMENTS = fopen([subjectpath '\' currid '\' subcode '_TrialComments.txt'],'w');
         fprintf(fileCOMMENTS,trialComments);
         fclose(fileCOMMENTS);
         
-        pause
-        clear CURRID
+        clear currid
     end
 end
